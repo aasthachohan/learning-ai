40c30bee899dc08ac0506de1c86233bfa35ef323
Aastha chohan <aasthachohan2014@gmail.com>
2026-04-29 23:04:11 +0530

    chore: file formating

--- phase-2-basic-rag/backend/utils/chunking.utils.js
+++ phase-2-basic-rag/backend/utils/chunking.utils.js
@@ -1,10 +1,10 @@
 // splitting of text
 function chunkingText(text, chunkSize = 120) {
-    const chunksArray = [];
-    for(let i=0; i < text.length; i=i+chunkSize) {
-        chunksArray.push(text.slice(i, i+chunkSize))
-    }
-    return chunksArray;
+  const chunksArray = [];
+  for (let i = 0; i < text.length; i = i + chunkSize) {
+    chunksArray.push(text.slice(i, i + chunkSize));
+  }
+  return chunksArray;
 }
 
 module.exports = { chunkingText };
