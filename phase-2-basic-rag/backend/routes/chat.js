2d77433abed9ab381c8877e092ed5a7734409bff
Aastha chohan <aasthachohan2014@gmail.com>
2026-05-03 19:07:50 +0530

    feat: added chat router

---
+++ phase-2-basic-rag/backend/routes/chat.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router();
+
+const { getRelevantChunks } = require("../services/rag.service");
+const { callLLM } = require("../services/llm.service");
+
+router.post("/", async (req, res) => {
+  try {
+    const userMessage = req.body.message;
+    const chunks = getRelevantChunks(userMessage);
+    const context = chunks.join("\n");
+
+    const prompt = `You must answer ONLY from the context below.
+    If the answer is not present, reply with "I don't know".
+    Context: ${context}
+    Question: ${userMessage}
+    `;
+
+    const reply = await callLLM(prompt);
+    res.json({ reply });
+  } catch (err) {
+    console.error("FULL ERROR:", err);
+    console.error("ERROR RESPONSE:", err.response?.data);
+
+    res.status(500).json({
+      error: err.message,
+      details: err.response?.data,
+    });
+  }
+});
+
+module.exports = router;
