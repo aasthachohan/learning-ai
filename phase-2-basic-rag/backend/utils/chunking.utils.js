78888517536fdf462fb67529cf709c7e822d4d4c
Aastha chohan <aasthachohan2014@gmail.com>
2026-05-03 19:07:24 +0530

    chore: updated chunk size to retrive proper response

--- phase-2-basic-rag/backend/utils/chunking.utils.js
+++ phase-2-basic-rag/backend/utils/chunking.utils.js
@@ -1,5 +1,5 @@
 // splitting of text
-function chunkingText(text, chunkSize = 120) {
+function chunkingText(text, chunkSize = 100) {
   const chunksArray = [];
   for (let i = 0; i < text.length; i = i + chunkSize) {
     chunksArray.push(text.slice(i, i + chunkSize));
