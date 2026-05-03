a191c0074027c151756953105cf4b2df73055307
Aastha chohan <aasthachohan2014@gmail.com>
2026-05-03 19:07:02 +0530

    chore: updated llm and rag services

--- phase-2-basic-rag/backend/services/rag.service.js
+++ phase-2-basic-rag/backend/services/rag.service.js
@@ -4,13 +4,18 @@ const { chunkingText } = require("../utils/chunking.utils");
 
 const knowledgeFilePath = path.join(__dirname, "../data/knowledge-source.txt");
 
-function getRelevantChunks(query) {
-  const textInStoredFile = fs.readFileSync(knowledgeFilePath, "utf-8");
-  const convertTextIntoChunks = chunkingText(textInStoredFile);
-  const filteredText = convertTextIntoChunks.filter((chunk) => {
-    chunk.toLowerCase().includes(query.toLowerCase());
-  });
-  return filteredText;
+function getRelevantChunks(userQuery) {
+  const storedFileText = fs.readFileSync(knowledgeFilePath, "utf-8");
+  const storedFileChunks = chunkingText(storedFileText);
+
+  const finalUserInputWordsArray =
+    userQuery.toLowerCase().match(/\b(\w{3,})\b/g) || [];
+
+  return storedFileChunks.filter((chunk) =>
+    finalUserInputWordsArray.some((userInputWord) =>
+      chunk.toLowerCase().includes(userInputWord)
+    )
+  );
 }
 
 module.exports = { getRelevantChunks };
