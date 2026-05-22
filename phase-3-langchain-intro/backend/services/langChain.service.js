5184acdc26671c9dced509f23e3f131586c1afde
Aastha chohan <aasthachohan2014@gmail.com>
2026-05-22 23:39:57 +0530

    feat: added new langchain service

--- phase-3-langchain-intro/backend/services/langChain.service.js
+++ phase-3-langchain-intro/backend/services/langChain.service.js
@@ -1,1 +1,35 @@
-// main file - read file, split file into chunks, create context, format prompt, invoke gemini model
+// This file only does the following 5 things and nothing else -
+// Read source text
+// Split into chunks
+// Create context
+// Build prompt
+// Call Gemini via LangChain
+import fs from "fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
+import geminiModel from "../config/gemini.config.js";
+import chatPrompt from "../prompt/chat.prompt.js";
+import { splitText } from "../utils/textSplitter.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export async function getResponseUsingLangchain(req, res) {
+    try {
+        const { requestedMessage } = req.body;
+        const filePath = path.join(__dirname, "../data/knowledgeSource.txt");
+        const sourceText = await fs.readFile(filePath, 'utf-8');
+        const chunks = await splitText(sourceText);
+        const context = chunks.join('\n');
+        const finalPrompt = await chatPrompt.format({context: context, question: requestedMessage});
+        const invokeModelAndReceiveResponse = await geminiModel.invoke(finalPrompt);
+        return res.json({
+            answer: invokeModelAndReceiveResponse.content
+        });
+    } catch(error) {
+        console.log(error);
+        return res.status(500).json({
+            error: error.requestedMessage
+        });
+    }
+}
